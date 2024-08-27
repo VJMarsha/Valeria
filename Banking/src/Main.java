e7512ea1dd0f02a64f0346f3ba3103bdceda4f80
VJMarsha <168444824+VJMarsha@users.noreply.github.com>
2024-08-27 07:50:48 +0200

    Add files via upload

---
+++ Banking/src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        // Creating an instance of the Bank class
+        Bank myBankAccount = new Bank("VJ", 230357);
+        myBankAccount.display(); // Invoking the display method
+    }
+}
